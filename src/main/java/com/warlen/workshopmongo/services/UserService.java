9bfc02223adff5d078262fbe4e12808f81c993fd
Warlen <warlensa007@gmail.com>
2025-05-30 16:07:55 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/warlen/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.warlen.workshopmongo.services;
+
+import com.warlen.workshopmongo.domain.User;
+import com.warlen.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll(){
+    return repo.findAll();
+    }
+}
